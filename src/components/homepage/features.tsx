011ef5e4e2131548e86ce9afbe76b97b6d0cd185
Krzysztof Chwastowski <qetzin23@gmail.com>
2025-12-08 23:26:43 +0100

    4.4

--- src/components/homepage/features.tsx
+++ src/components/homepage/features.tsx
@@ -1,7 +1,7 @@
 export default function Features(){
     return(<section className="overflow-hidden py-14 lg:py-28">
         <div className="container">
-            <p>test</p>
+            
         </div>
     </section>);
 }
---
+++ src/data/home-data.ts
@@ -0,0 +1,23 @@
+export const features = [
+    {
+        id: 1,
+        icon:'/icons/feature1.png',
+        title:'Najnowsze technologie',
+        description:'Korzystamy z najnowszych technologii, aby zapewnić maksymalną wydajność i jakość',
+        delay:'.2s'
+    },
+    {
+        id: 2,
+        icon:'/icons/feature2.png',
+        title:'Łatwość obsługi',
+        description:'Korzystamy z najnowszych technologii, aby zapewnić maksymalną wydajność i jakość',
+        delay:'.5s'
+    },
+    {
+        id: 3,
+        icon:'/icons/feature3.png',
+        title:'Wsparcie na medal',
+        description:'Korzystamy z najnowszych technologii, aby zapewnić maksymalną wydajność i jakość',
+        delay:'.8s'
+    }
+];
