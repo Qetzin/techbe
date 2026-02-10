52784b6a0d6df08422ac410de001940b6e92fdee
Krzysztof Chwastowski <qetzin23@gmail.com>
2026-02-10 19:54:48 +0100

    4.6

--- src/data/home-data.ts
+++ src/data/home-data.ts
@@ -21,3 +21,30 @@ export const features = [
         delay:'.8s'
     }
 ];
+
+export const slides = [
+  {
+    id: 1,
+    bgSrc: "/images/baner1.jpg",
+    subtitle: "Najlepsze rozwiązanie dla IT",
+    title1: "Dokumentacja finansów",
+    title2: "Dla Twojego biznesu",
+    text: "Profesjonalne narzędzia, dzięki którym z łatwością utworzysz historię przychodów/wydatków oraz zaplanujesz finanse dla Twojej firmy.",
+  },
+  {
+    id: 2,
+    bgSrc: "/images/baner2.jpg",
+    subtitle: "Najlepsze rozwiązanie dla IT",
+    title1: "Dokumentacja finansów",
+    title2: "Dla Twojego biznesu",
+    text: "Profesjonalne narzędzia, dzięki którym z łatwością utworzysz historię przychodów/wydatków oraz zaplanujesz finanse dla Twojej firmy.",
+  },
+  {
+    id: 3,
+    bgSrc: "/images/baner3.jpg",
+    subtitle: "Najlepsze rozwiązanie dla IT",
+    title1: "Dokumentacja finansów",
+    title2: "Dla Twojego biznesu",
+    text: "Profesjonalne narzędzia, dzięki którym z łatwością utworzysz historię przychodów/wydatków oraz zaplanujesz finanse dla Twojej firmy.",
+  },
+];
